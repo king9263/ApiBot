3e9c1f71d5ef3ddfd6497692b72efb1b0dc50003
MasterErfan <MasterErfan@users.noreply.github.com>
2016-08-20 23:14:42 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'taylor' then -- Put everything you like :)
+  if matches[1]:lower() == 'master' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "taylor" then -- Put everything you like :)
+    if matches[2] == "master" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
