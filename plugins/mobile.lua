24b00cb3d479089c6a108d88c280c96cf078c776
Erfan Bozorgi <NeoErfan@users.noreply.github.com>
2016-09-01 18:25:13 +0430

    Add files via upload

---
+++ plugins/mobile.lua
@@ -0,0 +1,32 @@
+﻿--[[
+نمونه اصلی MOBILE اوپن شده توسط 
+@ValtMan
+@ubuntu_14 (main API)
+بدون محدودیت استفاده کنید بدون تبلیغات :D
+
+تشکر :)
+Our Channel : @UB_CH
+اگر بدون ذکر منبع پخش بشه دیگه اوپن نمیکنم
+]]
+
+
+
+local function run(msg, matches)
+local url = http.request("http://www.mobile.ir/phones/AjaxLatestPhonesJson.aspx")
+  local jdat = json:decode(url)
+  local text2 = ''
+  for i = 1 , #jdat do
+    text2 = text2..i
+    text2 = text2..' ) اسم مبایل : '..jdat[i].title
+	text2 = text2..'\nجزییات : http://www.mobile.ir'..jdat[i].url.."\n"
+    end
+  return reply_msg(msg.id, text2, ok_cb, false)
+end
+
+return {
+  description = "Get New Mobile Phones From Mobile.ir",
+  patterns = {
+    "^([Mm][Oo][Bb][Ii][Ll][Ee])$"
+  },
+  run = run
+}
