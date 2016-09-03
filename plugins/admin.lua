52c558c09d64d9f253595c894278dabbdfd4b7ff
Erfan Bozorgi <NeoErfan@users.noreply.github.com>
2016-09-03 18:37:09 +0430

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -262,7 +262,7 @@ end
 		receiver = get_receiver(msg)
 		reload_plugins(true)
 		post_msg(receiver, "", ok_cb, false)
-		return "<b>All Plugins And Detail Reloaded!</b>"
+		return "All Plugins And Detail Reloaded!"
 	end
 	--[[*For Debug*
 	if matches[1] == "vardumpmsg" and is_admin1(msg) then
