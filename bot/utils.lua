40879b5f7ef4a026239defc3315d1457fb93c025
Erfan Bozorgi <NeoErfan@users.noreply.github.com>
2016-09-04 09:03:14 +0430

    Add files via upload

--- bot/utils.lua
+++ bot/utils.lua
@@ -1272,10 +1272,14 @@ function get_receiver_api(msg)
     return '-100'..msg.to.id
   end
 end
-
 function send_api_msg(msg, receiver, text, disable_web_page_preview, markdown,inline_text,inline_url)
- local api_key = '237076182:AAEgPJBsfD9OFqHRT2zQyWByn9_RM-S0djw'
- local url_api = 'https://api.telegram.org/bot'..api_key..'/sendMessage?chat_id='..receiver..'&text='..URL.escape(text)
+  
+  
+  local api_key = 'توکن ربات'
+  
+  
+  local url_api = 'https://api.telegram.org/bot'..api_key
+      ..'/sendMessage?chat_id='..receiver..'&text='..URL.escape(text)
   if disable_web_page_preview == true then
     url_api = url_api..'&disable_web_page_preview=true'
   end
@@ -1290,6 +1294,14 @@ end
   local dat, res = https.request(url_api)
   local test = print(url_api)
   if res == 400 then
-    reply_msg(msg.id, 'Error 400.\nWhat ever that means...', ok_cb, true)
+    reply_msg(msg.id, 'بو عنت همه جارو برداشته (تو کیبورد ریدی)...', ok_cb, true)
   end
 end
+function send_api_keyboard(msg, receiver, text, keyboard)
+  local api_key = 'توکن ربات'
+  local url_api = 'https://api.telegram.org/bot'..api_key..'/sendMessage?chat_id='.. receiver..'&parse_mode=markdown&&text='..URL.escape(text)..'&disable_web_page_preview=true&reply_markup='..json:encode(keyboard)
+  local dat, res = https.request(url_api)
+  if res == 400 then
+    reply_msg(msg.id, 'بو عنت همه جارو برداشته (تو کیبورد ریدی)...', ok_cb, true)
+  end
+  end
